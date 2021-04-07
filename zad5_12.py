6d32102c6daf7eb354b8c3a7ed8e64bc444be1db
Dybowskii <dybo12229@wp.pl>
2021-04-07 15:37:21 +0200

    brakujacy element

---
+++ zad5_12.py
@@ -0,0 +1,7 @@
+miesiace=["styczen","luty","marzec","kwiecien","maj","czerwiec","lipiec","sierpien","wrzesien","pazdziernik","listopad","grudzien"]
+miesiac = (miesiac for miesiac in miesiace)
+print(miesiac)
+print(next(miesiac))
+print(next(miesiac))
+print(next(miesiac))
+print(next(miesiac))
